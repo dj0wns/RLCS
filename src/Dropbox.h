684eea2746aa5bec2174d2a0a8590c26b9fddf2e
Andrew Boyan <anfu08@gmail.com>
2017-01-20 19:32:49 -0700

    Made base class functions public.

--- src/Dropbox.h
+++ src/Dropbox.h
@@ -1,7 +1,20 @@
 #ifndef DROPBOX_H
 #define DROPBOX_H
 
+class Dropbox{
 
+public:
+	int Download(FILE &out_file);
+	int Upload(FILE &in_file);
+	
+	int Remove(char* file_name);
+
+	int List_Files(std::vector<std::string> &file_list);
+
+	int Space_Free();
+	int Space_Used();
+
+};
 
 
 #endif
